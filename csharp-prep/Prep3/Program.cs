52d8f4b427da18a27e5f95f55e8aba5977f99f09
Timothy L Anderson <tim4tat.mac@Timothys-MacBook-Air.local>
2026-09-21 18:07:18 -0600

    complete prep3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,9 +1,49 @@
 using System;
+using System.Collections.Concurrent;
+using System.Runtime.Serialization;
 
 class Program
 {
     static void Main(string[] args)
-    {
-        Console.WriteLine("Hello Prep3 World!");
+    {    
+        string on = "y";
+
+        //Console.Write("What is the magic number? ");
+        //string magicNumberString = Console.ReadLine();
+        //float magicNumber = float.Parse(magicNumberString);
+
+        do{
+        string correct = "No";
+        int count = 0;
+        
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1, 100);
+            do{
+                Console.Write("What is your number? ");
+                string guessString = Console.ReadLine();
+                float guess = float.Parse(guessString);
+
+                if (guess > magicNumber)
+                {
+                    Console.WriteLine("lower");
+                    count ++;
+                }
+                else if (guess < magicNumber)
+                {
+                    Console.WriteLine("Higher");
+                    count ++;
+                }
+                else
+                {
+                    Console.WriteLine("You got it!!");
+                    correct = "Yes";
+                    Console.WriteLine($"it took you {count} tries");
+
+                    Console.Write("Play again? y/n ");
+                    on = Console.ReadLine();
+                }
+
+            } while (correct == "No");
+        } while (on == "y");
     }
 }
